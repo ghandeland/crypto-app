02aa39ac6336794b74d209253dc7950baf2d87a1
ghandeland <gabriel.handeland@gmail.com>
2021-04-15 17:43:09 +0200

    Added read/write check for shared preferences in splash screen activity

--- app/src/main/java/no/kristiania/pgr208_1/pgr208_1_exam/SplashScreenActivity.kt
+++ app/src/main/java/no/kristiania/pgr208_1/pgr208_1_exam/SplashScreenActivity.kt
@@ -1,28 +1,55 @@
 package no.kristiania.pgr208_1.pgr208_1_exam
 
-import android.content.Intent
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
-import no.kristiania.pgr208_1.pgr208_1_exam.databinding.ActivityMainBinding
 import no.kristiania.pgr208_1.pgr208_1_exam.databinding.ActivitySplashScreenBinding
-import java.lang.Thread.sleep
+
+private const val TRANSACTION_FLAG_KEY = "no.kristiania.pgr208_1.exam.NEW_TRANSACTION_KEY"
 
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
+        // Check shared preferences for flag that indicates if initial transaction to DB should be executed
+        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+        val makeInitialTransaction = sharedPref.getBoolean(TRANSACTION_FLAG_KEY, true)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+        if(makeInitialTransaction) {
+            makeTransaction()
+            toggleTransaction(false)
+        }
+
+
+
+
+        Log.d("sharedpref", "Does contain: $doesContain")
+
+
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 3000)
     }
+
+    private fun makeTransaction() {
+        TODO("Not yet implemented")
+    }
+
+    private fun toggleTransaction(makeNextTransaction: Boolean) {
+        val sharedPref = this?.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putBoolean(TRANSACTION_FLAG_KEY, makeNextTransaction)
+            apply()
+        }
+    }
+
 }
